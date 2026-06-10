8f4fbc3171610b67b07c5a88414b9b35a79f3eb0
Harman Sihag <harman.sihag.edu@gmail.com>
2026-06-10 23:19:02 +0530

    Reduce errors PreciseCountdown.py
    Added while loop to make sure only numbers are added. Any word input previously would give an error. With the change, it asks the user again until a valid input is given instead of giving an error and crashing the program.

--- PreciseCountdown.py
+++ PreciseCountdown.py
@@ -1,13 +1,20 @@
 import time      # A PRECISE COUNTDOWN TIMER THAT REDUCES ANY TIME ERROR ACCUMULATION USING time.time()
-duration = int((input("Enter the Timer Count: ")))
+
+while True : 
+    duration = (input("Enter the Timer Count in seconds: "))
+    if not duration.isdigit() :       #  MAKES SURE ONLY WORDS ARE ENTERED
+        print(f"Words are not allowed.")
+    else: 
+        break
+x = int(duration)
 uniclock = time.time()
 
-for (i) in range(duration, 0, -1): 
+for (i) in range(x, 0, -1): 
     seconds = i% 60
     minutes = int(i / 60) % 60
     hours = int( i / 60) 
     print(f"{hours:02}:{minutes:02}:{seconds:02}")
-    next_tick= uniclock + (duration - i + 1 )  # the update in the uniclock that adds the seconds passed to the uniclock 
+    next_tick= uniclock + (x - i + 1 )  # the update in the uniclock that adds the seconds passed to the uniclock 
     error_in_time = next_tick - time.time() 
     if error_in_time > 0: #makes sure the program runs immediately after a freeze or lag if the lag happens around the ending of countdown
         time.sleep(error_in_time)
